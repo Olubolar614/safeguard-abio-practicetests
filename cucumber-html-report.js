0bdf26b00fe9bea5ff4caf272c0548d6f55da04d
HUMAYUN MUSHTAQ <131605337+Humayun0008@users.noreply.github.com>
2024-03-18 20:35:54 +0500

    Add the reporter in Cypress Project

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,16 +1,27 @@
 const report = require("multiple-cucumber-html-reporter");
+
 report.generate({
-  jsonDir: "jsonlogs", // ** Path of .json file **//
-  reportPath: "./reports/cucumber-htmlreport.html",
+  jsonDir: "./cypress/reports/cucumber-json",
+  reportPath: "./cypress/reports/html-multi-report",
   metadata: {
     browser: {
       name: "chrome",
-      version: "121",
+      version: "60",
     },
     device: "Local test machine",
     platform: {
-      name: "Mac",
-      version: "Sonoma",
+      name: "ubuntu",
+      version: "16.04",
     },
+  },
+  customData: {
+    title: "Orange Hrm Testcases Run info",
+    data: [
+      { label: "Project", value: "Orange Hrm" },
+      { label: "Release", value: "1.2.3" },
+      { label: "Cycle", value: "B11221.34321" },
+      { label: "Execution Start Time", value: "Mar 8th 2024, 02:31 PM EST" },
+      { label: "Execution End Time", value: "Mar 8th 2024, 02:56 PM EST" },
+    ],
   },
 });
